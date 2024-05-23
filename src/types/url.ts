c2a62952e65942af4875d1167d9ff2ef3243afe2
Anthony Collier <hello@acollier.dev>
2024-05-23 03:50:45 -0400

    fix(types): update typings

--- src/types/url.ts
+++ src/types/url.ts
@@ -1,24 +1,8 @@
-export type AnalysisData = {
-  title: string;
-  description: string;
-  sourceUrl: string;
-  destinationUrl: string;
-  redirects: UrlRedirectData[];
-};
+import { AnalyzedUrlRedirect } from '@safepeek/utils';
 
 export interface UrlAnalysisInputData {
   guildId: bigint | null;
   userId: bigint;
   channelId: bigint;
-  urls: UrlRedirectData[];
-}
-
-export interface UrlRedirectData {
-  rawUrl: string;
-  meta?: AnalyzedUrlRedirectMetadata;
-}
-
-export interface AnalyzedUrlRedirectMetadata {
-  title?: string;
-  description?: string;
+  urls: AnalyzedUrlRedirect[];
 }
