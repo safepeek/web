f1a77a308da34506edf91be2be51564d4a44208a
Anthony Collier <hello@acollier.dev>
2024-05-23 04:01:36 -0400

    fix: update typings
    - we imported from the wrong location/package

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,5 +1,5 @@
 import { load } from 'cheerio';
-import { type AnalysisData, UrlRedirectData } from '@/types/url';
+import { AnalysisData, AnalyzedUrlRedirect } from '@safepeek/utils';
 
 type FetcherOpts = {
   follow?: boolean;
@@ -51,7 +51,7 @@ const fetchWithRedirects = async (url: string) => {
   let currentUrl = url;
   let response: Response;
   let metadata: MetadataResponse;
-  const urls: UrlRedirectData[] = [];
+  const urls: AnalyzedUrlRedirect[] = [];
   let redirectCount = 0;
 
   try {
